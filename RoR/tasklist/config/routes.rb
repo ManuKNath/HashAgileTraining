69a0fb777bb7088fa5617ff85a353c1e7e026d7c
Manu K Nath <manuknath@gmail.com>
2022-08-22 09:47:43 +0530

    Internationalization added for French

--- RoR/tasklist/config/routes.rb
+++ RoR/tasklist/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   #get 'home/index'
-  devise_for :logins
-  resources :users
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  # your routes here...
+    devise_for :users
+    resources :users
+    resources :tasks
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root 'home#index'
+    # Defines the root path route ("/")
+    root 'home#index'
+  end
+  
 end
